b177c92d0b1aad53c1fbb2b94e87ca543ffa5abc
LogzatioStudios150 <32424374+LogzatioStudios150@users.noreply.github.com>
2024-07-02 14:25:14 -0500

    Identifier parameter is auto filled with name of asset.
    Identifier is filled with asset name when creating LuxCreationImportedPart. The Offset part canidate parents are also setup.

--- Source/LuxorGame/Private/LuxCreationImportedPart.cpp
+++ Source/LuxorGame/Private/LuxCreationImportedPart.cpp
@@ -7,5 +7,10 @@ ULuxCreationImportedPart::ULuxCreationImportedPart() {
     this->bDisableBreastSwing = false;
     this->ColorEditFlags.AddDefaulted(4);
     this->UnderwearColorFlags.AddDefaulted(4);
+	this->Identifier = this->GetFName();
+	if (this->Identifier.ToString().Contains("extra")) {
+		this->OffsetPart.bIsValid = true;
+	}
+
 }
 
--- Source/LuxorGame/Private/LuxOffsetPart.cpp
+++ Source/LuxorGame/Private/LuxOffsetPart.cpp
@@ -3,5 +3,29 @@
 FLuxOffsetPart::FLuxOffsetPart() {
     this->bIsValid = false;
     this->DefaultParent = ELuxOffsetParentBone::EPB_HEAD;
+	this->CandidateParents.AddDefaulted(17);
+
+	this->CandidateParents[1].Bone = ELuxOffsetParentBone::EPB_CLAVICLE_R;
+	this->CandidateParents[2].Bone = ELuxOffsetParentBone::EPB_CLAVICLE_L;
+	this->CandidateParents[3].Bone = ELuxOffsetParentBone::EPB_SHOULDER_R;
+	this->CandidateParents[4].Bone = ELuxOffsetParentBone::EPB_SHOULDER_L;
+	this->CandidateParents[5].Bone = ELuxOffsetParentBone::EPB_BREAST;
+	this->CandidateParents[6].Bone = ELuxOffsetParentBone::EPB_ARM_R;
+	this->CandidateParents[7].Bone = ELuxOffsetParentBone::EPB_ARM_L;
+	this->CandidateParents[8].Bone = ELuxOffsetParentBone::EPB_HAND_R;
+	this->CandidateParents[9].Bone = ELuxOffsetParentBone::EPB_HAND_L;
+	this->CandidateParents[10].Bone = ELuxOffsetParentBone::EPB_WAIST;
+	this->CandidateParents[11].Bone = ELuxOffsetParentBone::EPB_THIGH_R;
+	this->CandidateParents[12].Bone = ELuxOffsetParentBone::EPB_THIGH_L;
+	this->CandidateParents[13].Bone = ELuxOffsetParentBone::EPB_KNEE_R;
+	this->CandidateParents[14].Bone = ELuxOffsetParentBone::EPB_KNEE_L;
+	this->CandidateParents[15].Bone = ELuxOffsetParentBone::EPB_FOOT_R;
+	this->CandidateParents[16].Bone = ELuxOffsetParentBone::EPB_FOOT_L;
+
+	for (int i = 0; i < 17; i++)
+	{
+		this->CandidateParents[i].DefaultRotation = FVector(0, 0, 0);
+		this->CandidateParents[i].DefaultTranslation = FVector(0, 0, 0);
+	}
 }
 
